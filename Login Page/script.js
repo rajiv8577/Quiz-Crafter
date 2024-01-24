2985b446db7e19eb331e584450f829df6dcd08a5
rajiv@8577 <rajivmalviya8577@gmail.com>
2024-01-24 23:55:45 +0530

    deleted login

--- Login Page/script.js
+++
@@ -1,7 +0,0 @@
-document.getElementById('signUp').addEventListener('click', function () {
-    document.getElementById('container').classList.add('right-panel-active');
-  });
-  
-  document.getElementById('signIn').addEventListener('click', function () {
-    document.getElementById('container').classList.remove('right-panel-active');
-  });
